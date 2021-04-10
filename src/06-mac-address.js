c24b63015e29c9bfb118f58f95843dcba5967ecf
zlobinstas <zlobinstas@bk.ru>
2021-04-10 19:08:14 +0200

    feat: add task 6 mac-address

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  // eslint-disable-next-line eqeqeq
+  return n.match(/[A-F0-9][A-F0-9]/g).join('-') == n;
 }
 
 module.exports = isMAC48Address;
